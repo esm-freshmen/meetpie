39dd059442171a9816d70f2c14065472b1b0206e
reina.saito <reina.saito@esm.co.jp>
2024-05-28 20:03:22 +0900

    背景色、文字色の設定をした

--- src/app/attendance-table/page.tsx
+++ src/app/attendance-table/page.tsx
@@ -1,17 +1,19 @@
+import styles from './page.module.css';
+
 export default function Page() {
   return (
     <div>
-      <div>タイトル</div>
-      <div>説明</div>
-      <div>イベント詳細</div>
-      <table>
+      <h1 className={styles.h1}>若手交流会</h1>
+      <div>ここに説明が入ります</div>
+      <br />
+      <table className={styles.table}>
         <thead>
           <tr>
-            <td></td>
-            <td>12/11</td>
-            <td>12/12</td>
-            <td>12/13</td>
-            <td>12/14</td>
+            <td className={styles.td}></td>
+            <td className={styles.td}>12/14</td>
+            <td className={styles.td}>12/11</td>
+            <td className={styles.td}>12/12</td>
+            <td className={styles.td}>12/13</td>
           </tr>
         </thead>
         <tbody>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
 import Image from 'next/image';
+import styles from './page.module.css';
 
 export default function Home() {
-  return <>Home</>;
+  return (
+    <main className={styles.main}>
+      <div>Home</div>
+    </main>
+  );
 }
