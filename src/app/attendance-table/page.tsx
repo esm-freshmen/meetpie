e436c512c9eb1ad5dec5fcb276c77bf0b6f6cb59
kiyoharu-hijiriyama <kiyoharu.hijiriyama@esm.co.jp>
2024-05-29 11:07:58 +0000

    出欠表をコンポーネントとして抽出

--- src/app/attendance-table/page.tsx
+++ src/app/attendance-table/page.tsx
@@ -1,38 +1,11 @@
-import styles from './page.module.css';
+import AttendanceTable from '@/components/attendanceTable';
 
 export default function Page() {
   return (
-    <div>
-      <h1 className={styles.h1}>若手交流会</h1>
-      <div>ここに説明が入ります</div>
-      <br />
-      <table className={styles.table}>
-        <thead>
-          <tr>
-            <td className={styles.td}></td>
-            <td className={styles.td}>12/14</td>
-            <td className={styles.td}>12/11</td>
-            <td className={styles.td}>12/12</td>
-            <td className={styles.td}>12/13</td>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>10:00</td>
-            <td>1人</td>
-            <td>4人</td>
-            <td>1人</td>
-            <td>2人</td>
-          </tr>
-          <tr>
-            <td>11:00</td>
-            <td>4人</td>
-            <td>5人</td>
-            <td>2人</td>
-            <td>1人</td>
-          </tr>
-        </tbody>
-      </table>
+    <div className="flex items-center flex-col">
+      <h1 className="text-5xl mb-5">若手交流会</h1>
+      <div className="mb-5">ここに説明が入ります</div>
+      <AttendanceTable />
     </div>
   );
 }
---
+++ src/components/attendanceTable.tsx
@@ -0,0 +1,33 @@
+function AttendanceTable() {
+  return (
+    <table className="">
+      <thead>
+        <tr>
+          <td className=""></td>
+          <td className="">12/14</td>
+          <td className="">12/11</td>
+          <td className="">12/12</td>
+          <td className="">12/13</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>10:00</td>
+          <td>1人</td>
+          <td>4人</td>
+          <td>1人</td>
+          <td>2人</td>
+        </tr>
+        <tr>
+          <td>11:00</td>
+          <td>4人</td>
+          <td>5人</td>
+          <td>2人</td>
+          <td>1人</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+}
+
+export default AttendanceTable;
