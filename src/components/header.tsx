1c1eaea9eb98ce8bbb4485c28e144e07c7b1bc0b
kiyoharu-hijiriyama <kiyoharu.hijiriyama@esm.co.jp>
2024-06-19 10:38:35 +0000

    ヘッダー追加

---
+++ src/components/header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="bg-[#651616] h-[68px] flex items-center">
+      <h1 className="text-[#ffffff] text-3xl ml-4">Meet Pie</h1>
+    </header>
+  );
+}
+
+export default Header;
