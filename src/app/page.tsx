c9da82fd5551542213c10a98894f25a1934cf370
shumatsu51 <shuhei.matsushima@esm.co.jp>
2026-04-13 17:55:50 +0900

    ボタンとチェックボックスのstyleを刷新した

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -97,7 +97,7 @@ export default function Home() {
               </p>
             )}
           </div>
-          <button>作成</button>
+          <button className="btn btn-primary">作成</button>
         </form>
       </main>
     </div>
