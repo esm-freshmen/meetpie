7c2beeeef29d4c7c4b6d323d7375a71d6cf49408
HarutoTakeuchi <haruto.takeuchi@esm.co.jp>
2026-05-14 23:47:45 +0900

    wip: infra

--- infra/index.ts
+++ infra/index.ts
@@ -1,9 +1,52 @@
 import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
 // Create an AWS resource (S3 Bucket)
 const bucket = new aws.s3.Bucket("my-bucket");
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+// IAM role for Lambda
+const lambdaRole = new aws.iam.Role("meetpie-lambda-role", {
+    assumeRolePolicy: aws.iam.assumeRolePolicyForPrincipal({
+        Service: "lambda.amazonaws.com",
+    }),
+});
+
+new aws.iam.RolePolicyAttachment("meetpie-lambda-basic-execution", {
+    role: lambdaRole.name,
+    policyArn: aws.iam.ManagedPolicy.AWSLambdaBasicExecutionRole,
+});
+
+// Lambda function
+const lambdaFn = new aws.lambda.Function("meetpie-lambda", {
+    runtime: aws.lambda.Runtime.NodeJS22dX,
+    role: lambdaRole.arn,
+    handler: "index.handler",
+    code: new pulumi.asset.AssetArchive({
+        "index.mjs": new pulumi.asset.StringAsset(`
+export const handler = async (event) => {
+    return {
+        statusCode: 200,
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ message: "Hello from meetpie Lambda!" }),
+    };
+};
+        `.trim()),
+    }),
+});
+
+// Function URL (no auth for public access)
+const functionUrl = new aws.lambda.FunctionUrl("meetpie-lambda-url", {
+    functionName: lambdaFn.name,
+    authorizationType: "NONE",
+    cors: {
+        allowCredentials: false,
+        allowOrigins: ["*"],
+        allowMethods: ["*"],
+        allowHeaders: ["*"],
+    },
+});
+
+export const lambdaFunctionUrl = functionUrl.functionUrl;
